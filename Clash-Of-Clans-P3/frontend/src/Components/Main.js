f16e20a6f5cce0bdfcf932c17b4a4bc9bb8f68ec
Jmajane <jordan.majane@outlook.com>
2022-05-15 16:21:49 -0400

    Main.js component created

--- Clash-Of-Clans-P3/frontend/src/Components/Main.js
+++ Clash-Of-Clans-P3/frontend/src/Components/Main.js
@@ -4,12 +4,67 @@ import { Route, Routes } from 'react-router-dom';
 import PlayerSearch from '../Pages/PlayerSearch';
 import PlayerStats from '../Pages/PlayerStats';
 
+require('dotenv').config()
+
 function Main(props) {
     const [player, setPlayer] = useState(null)
 
     const URL = "https://api.clashofclans.com/v1/players/";
+
+    const key = process.env.API_KEY
+
+    const getPlayer = () => {
+        fetch(URL)
+        .then(response => response.json())
+        .then((result) => setPlayer(result))
+    }
+
+    const createPlayer = async (onePlayer) => {
+        await fetch(URL, {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(onePlayer)
+        });
+        getPlayer();
+    };
+
+    const updatePlayer = async (onePlayer, id) => {
+        await fetch(URL + id, {
+            method: "put",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        })
+        getPlayer()
+    }
+
+    const deletePeople = async (id) => {
+        await fetch(URL + id, {
+            method: "delete",
+        })
+        getPlayer()
+    }
+
+    useEffect(() => getPlayer(), []);
+
+
+
   return (
-    <div>Main</div>
+    <main>
+        <Routes>
+            <Route path='/' element={<PlayerSearch 
+            player={player}
+            createPlayer={createPlayer} />
+            } />
+            <Route path='/player/:id' element={<PlayerStats 
+            player={player}
+            updatePlayer={updatePlayer}
+            deletePeople={deletePeople} />
+            } />
+        </Routes>
+    </main>
   )
 }
 
